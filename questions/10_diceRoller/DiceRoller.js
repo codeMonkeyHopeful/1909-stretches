75960dcc74cc9053da9668f8729310ef9c75f7d1
Ryan Jasinski <jasinski.17@gmail.com>
2019-10-23 18:59:31 -0400

    solution

--- questions/10_diceRoller/DiceRoller.js
+++ questions/10_diceRoller/DiceRoller.js
@@ -4,6 +4,22 @@
 
 class DiceRoller {
   //YOUR CODE
+  constructor(sides, die) {
+    if (sides > 0 && die > 0) {
+      this.sides = sides;
+      this.die = die;
+      this.history = [];
+    } else {
+      throw new Error('no good');
+    }
+  }
+  roll() {
+    for (let i = 0; i < this.die; i++) {
+      let roll = Math.floor(Math.random() * this.sides);
+      this.history.push(roll);
+      return roll;
+    }
+  }
 }
 
 module.exports = { DiceRoller };
